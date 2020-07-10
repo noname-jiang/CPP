c70c0e0fdaf4b29437413acfd2c7b8b6c28aca9a
noname-pika <65858571+noname-pika@users.noreply.github.com>
2020-07-10 12:02:36 +0800

    Update 001.cpp

--- 基础习题/001.cpp
+++ 基础习题/001.cpp
@@ -1,1 +1,24 @@
 
+//编写一个程序，要求用户输入小时数和分钟数。
+//在main()函数中，将这两个值传递给一个void函数，后者以下面这样的格式显示这两个值
+//Enter the number of hours:9
+//Enter the number of minutes:28
+//Time:9:28
+#include<iostream>
+using namespace std;
+
+void print(int a, int b)
+{
+	cout << "Time:" << a << ":" << b;
+}
+
+int main()
+{
+	int num1{}, num2{};
+	cout << "Enter the number of hours:";
+	cin >> num1;
+	cout << "Enter the number of minutes:";
+	cin >> num2;
+	print(num1, num2);
+	return 0;
+}
